bcbe40c7e6d42fd1d14d305afd504be9d3af9422
Githsara Senarathne <88274577+Githsara99@users.noreply.github.com>
2024-12-09 15:55:16 +0530

    Add main folders

--- backend/backend/buymeanoodle/urls.py
+++ backend/backend/buymeanoodle/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+# add main folder routes
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
