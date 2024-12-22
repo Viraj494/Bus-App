42fb6db434a6e2df0d8858dddc8a0aa507fde4c8
Githsara Senarathne <88274577+Githsara99@users.noreply.github.com>
2024-12-22 09:05:00 +0530

    Add url patterns

--- backend/backend/core/urls.py
+++ backend/backend/core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-
+# import views from core
 from . import views
 
 
 app_name = 'core'
 
-
+# add core file paths
 urlpatterns = [
     path('', views.admin_home, name="admin_home"),
     path('index', views.index, name='index'),
