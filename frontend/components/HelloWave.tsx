883bb6dd9d4a200dd8110523bc5fc3231713f663
Thanusan <it21175848@my.sliit.lk>
2025-02-22 13:34:38 +0530

    add more bus data and locations

--- frontend/components/HelloWave.tsx
+++ frontend/components/HelloWave.tsx
@@ -12,6 +12,14 @@ const HelloWave = () => {
   const buses = [
     { id: "1", name: "Bus 101", latitude: 37.7749, longitude: -122.4194 },
     { id: "2", name: "Bus 102", latitude: 37.7755, longitude: -122.4183 },
+    { id: "3", name: "Bus 103", latitude: 37.776, longitude: -122.4172 },
+    { id: "4", name: "Bus 104", latitude: 37.7771, longitude: -122.4161 },
+    { id: "5", name: "Bus 105", latitude: 37.7782, longitude: -122.415 },
+    { id: "6", name: "Bus 106", latitude: 37.7793, longitude: -122.414 },
+    { id: "7", name: "Bus 107", latitude: 37.7804, longitude: -122.413 },
+    { id: "8", name: "Bus 108", latitude: 37.7815, longitude: -122.412 },
+    { id: "9", name: "Bus 109", latitude: 37.7826, longitude: -122.411 },
+    { id: "10", name: "Bus 110", latitude: 37.7837, longitude: -122.41 },
   ];
 
   return (
