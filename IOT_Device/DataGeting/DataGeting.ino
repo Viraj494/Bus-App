26aa006109e576539b648c32d9620dd512615a9b
HarithRajapaksha <priyaharshanaharith@gmail.com>
2025-03-16 07:57:27 +0530

    Api error occur

--- IOT_Device/DataGeting/DataGeting.ino
+++ IOT_Device/DataGeting/DataGeting.ino
@@ -156,6 +156,13 @@ void loop() {
     String distance = "";
     String passengerCount = "";
 
+    // Find the "Distance" and "Passenger_Count" from the response
+    int distanceStart = response.indexOf("\"Distance\":\"");
+    if (distanceStart != -1) {
+      distanceStart += 12;  // Skip the "\"Distance\":\""
+      int distanceEnd = response.indexOf("\"", distanceStart);
+      distance = response.substring(distanceStart, distanceEnd);
+    }
 
     int passengerCountStart = response.indexOf("\"Passenger_Count\":\"");
     if (passengerCountStart != -1) {
